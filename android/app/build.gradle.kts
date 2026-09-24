2cea5521eea5c18926ea332eda227bfa38acac92
8cee <aycee0@gmail.com>
2026-09-25 00:09:06 +0200

    Android: prepare r12 translation ROM-patch build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.eightcee.dk64recomp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 11
-        versionName = "1.0.3-android-r11"
+        versionCode = 12
+        versionName = "1.0.3-android-r12"
 
         ndk {
             // 64-bit apenas (decisão do usuário: memória e tempo de build)
