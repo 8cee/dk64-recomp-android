eb730f3d7cf4b0b8bda494275017d319a50f08cf
8cee <aycee0@gmail.com>
2026-09-24 23:45:19 +0200

    Android: prepare r11 graphics and mod compatibility build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.eightcee.dk64recomp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 10
-        versionName = "1.0.3-android-r10"
+        versionCode = 11
+        versionName = "1.0.3-android-r11"
 
         ndk {
             // 64-bit apenas (decisão do usuário: memória e tempo de build)
