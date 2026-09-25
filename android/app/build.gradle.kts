c749fd3fba1c11600a329f1e6ecfe0d9fadb238e
8cee <aycee0@gmail.com>
2026-09-25 03:26:15 +0200

    Android: prepare r16 renderer/autoresolution build

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "com.eightcee.dk64recomp"
         minSdk = 26
         targetSdk = 34
-        versionCode = 15
-        versionName = "1.0.3-android-r15"
+        versionCode = 16
+        versionName = "1.0.3-android-r16"
 
         ndk {
             // 64-bit apenas (decisão do usuário: memória e tempo de build)
